0a31de4ec9fbbc8ed20de6f750a1d7a7447ad6c3
Tomgh <Tomgh@DESKTOP-PUJFAED>
2022-11-29 00:41:31 -0600

    Conversor a Json

---
+++ Conversor/src/mx/itson/conversor/Conversor.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mx.itson.conversor;
+
+import conversor.Main;
+
+/**
+ *
+ * @author Luis Bejarano
+ */
+public class Conversor {
+
+    /**
+     * Genera el objeto
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main main = new Main();
+        main.setVisible(true);
+        
+        
+    }
+    
+}
---
+++ Conversor/src/mx/itson/conversor/Datos.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.conversor;
+
+/**
+ *
+ * @author Luis Bejarano
+ */
+public class Datos {
+    public String nombre;
+    public String edad;
+    public String correo;
+    public String telefono;
+    public String direccion;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the edad
+     */
+    public String getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
